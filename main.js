84b64caf0cf4c3062e5a409ddffb0dc0d43a4e43
RiveraAdrian <adriancubyblock@gmail.com>
2018-12-01 20:46:36 -0600

    fixed image sizing, changed backgorund color of the UL i the contacts page, added a hover effect to the NAV bar, and changed the font on the paragraphs

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ var i;
 if (time > 17 || time < 6){//if time is between 5pm and 6am
   for(i=0; i<3; i++){
 document.getElementsByClassName("reasons")[i].style.fontSize = "25px";
-document.getElementsByClassName("reasons")[i].style.backgroundColor = "#8B0000";
-document.getElementsByClassName("reasons")[i].style.color = "white";
+document.getElementsByClassName("reasons")[i].style.backgroundColor = "#D8D8D8";
+document.getElementsByClassName("reasons")[i].style.color = "#E61A36";
+document.getElementsByClassName("reasons")[i].style.fontFamily= "Impact,Charcoal,sans-serif";
   }
 }
 // else default web page style
